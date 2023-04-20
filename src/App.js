161f08218613ed43acac3d7acb1448e61d2ce79d
molaraiche <molaraiche@molaraiche.com>
2023-04-20 13:31:24 +0000

    updates

--- src/App.js
+++ src/App.js
@@ -23,23 +23,26 @@ const App = () => {
       swal("Please add a todo first");
     } else {
       const response = await axios.post(
-        `https://fair-rose-caterpillar-yoke.cyclic.app/api/todos`,
-        { Todo: add }
+        `https://todo-moa.up.railway.app/api/todos/create`,
+        {
+          Todo: add,
+        }
       );
       console.log(response.data);
       setTodos([...Todos, { Todo: add }]);
       setAdd("");
     }
-    window.location.href = "https://todo-client-navy.vercel.app";
+    // window.location.href = "https://todo-client-navy.vercel.app";
   };
 
   // get Todos
   const getTodos = async () => {
     const response = await axios.get(
-      "https://fair-rose-caterpillar-yoke.cyclic.app/api/todos"
+      "https://todo-moa.up.railway.app/api/todos"
     );
     const data = await response.data;
     setTodos(data.data);
+    console.log(data);
   };
   useEffect(() => {
     getTodos();
@@ -60,9 +63,7 @@ const App = () => {
         swal("Poof! Your Todo has been deleted!", {
           icon: "success",
         });
-        axios.delete(
-          `https://fair-rose-caterpillar-yoke.cyclic.app/api/todos/${id}`
-        );
+        axios.delete(`https://todo-moa.up.railway.app/api/todos/${id}`);
         const newList = Todos.filter((todo) => todo._id !== id);
         setTodos(newList);
       } else {
@@ -76,16 +77,15 @@ const App = () => {
     e.preventDefault();
     console.log(editId);
     axios
-      .put(
-        `https://fair-rose-caterpillar-yoke.cyclic.app/api/todos/${editId}`,
-        { Todo: update }
-      )
+      .put(`https://todo-moa.up.railway.app/api/todos/${editId}`, {
+        Todo: update,
+      })
       .then(() =>
         setTodos(
           Todos.map((todo) => (todo._id === editId ? { Todo: update } : todo))
         )
       );
-    window.location.href = "https://todo-client-navy.vercel.app";
+    // window.location.href = "https://todo-client-navy.vercel.app";
     setUpdate("");
     setModal("modalOff");
   };
@@ -104,57 +104,66 @@ const App = () => {
         </form>
       </div>
       <div className="todosHolder">
-        {Todos.map((todo) => (
-          <div key={todo._id} className="cart">
-            <div>
-              <p> {todo.Todo} </p>
-              <p> {todo.Detail} </p>
-            </div>
-            <div className="mngBtns">
-              <button
-                className="updateBtn"
-                onClick={(e) => updateToggle(todo._id)}>
-                Update
-              </button>
-              <button
-                className="deleteBtn"
-                onClick={(e) => {
-                  DeleteTodo(e, todo._id);
-                }}>
-                Delete
-              </button>
-            </div>
-            <div id="myModal" className={modal}>
-              <div className="modal-content">
-                <div className="closeHolder">
-                  <button
-                    className="close"
-                    onClick={() => setModal("modalOff")}>
-                    X
-                  </button>
-                </div>
-                <form
-                  onSubmit={(e) => {
-                    updateHandler(e, todo._id);
+        {Todos.length === 0 ? (
+          <p
+            style={{
+              height: "auto",
+            }}>
+            Loading...
+          </p>
+        ) : (
+          Todos.map((todo) => (
+            <div key={todo._id} className="cart">
+              <div>
+                <p> {todo.Todo} </p>
+                <p> {todo.Detail} </p>
+              </div>
+              <div className="mngBtns">
+                <button
+                  className="updateBtn"
+                  onClick={(e) => updateToggle(todo._id)}>
+                  Update
+                </button>
+                <button
+                  className="deleteBtn"
+                  onClick={(e) => {
+                    DeleteTodo(e, todo._id);
                   }}>
-                  <input
-                    style={{ color: "black" }}
-                    type="text"
-                    value={update}
-                    placeholder="UpdateTodo"
-                    onChange={(e) => {
-                      setUpdate(e.target.value);
-                    }}
-                  />
+                  Delete
+                </button>
+              </div>
+              <div id="myModal" className={modal}>
+                <div className="modal-content">
+                  <div className="closeHolder">
+                    <button
+                      className="close"
+                      onClick={() => setModal("modalOff")}>
+                      X
+                    </button>
+                  </div>
+                  <form
+                    onSubmit={(e) => {
+                      updateHandler(e, todo._id);
+                    }}>
+                    <input
+                      style={{ color: "black" }}
+                      type="text"
+                      value={update}
+                      placeholder="UpdateTodo"
+                      onChange={(e) => {
+                        setUpdate(e.target.value);
+                      }}
+                    />
 
-                  <button type="submit" className="updateBtn">
-                    Update Todo
-                  </button>
-                </form>
+                    <button type="submit" className="updateBtn">
+                      Update Todo
+                    </button>
+                  </form>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          ))
+        )}
       </div>
     </div>
   );
